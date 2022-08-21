e0b27b12a4601ea61e8a1b4d11a1853159428303
djosh <darrenjcarrillo@gmail.com>
2022-08-21 14:13:55 -0700

    Accordion done

--- js/src/global-script.js
+++ js/src/global-script.js
@@ -372,6 +372,7 @@ $("[data-scroll-to]").click(function(e) {
 });
 
 // ---------------------------------------------------------------------------------------------------
+// SWIPER
 
 class SwiperDemo {
   constructor() {
@@ -525,3 +526,62 @@ The startTranslate doesn't exist on initial load so we use the initialSlide inde
 }
 
 const demo = new SwiperDemo();
+
+// ---------------------------------------------------------------------------------------------------
+// ACCORDION
+// const accordion = document.getElementsByClassName('content-wrap');
+
+// for (i=0; i<accordion.length; i++) {
+//   accordion[i].addEventListener('click', function () {
+//     this.classList.toggle('active')
+//   })
+// }
+
+$(".btn-toggle-show").click(toggleText(".inner-content"));
+
+function toggleText(element) {
+  return function(e){
+    $(this).parent().find(element).slideToggle(300);  
+    $(this).parent().find(element).toggleClass("para-hide");
+    $(this).find(".plus-minus").toggleClass("rotate");
+  }
+}
+
+
+// FAQ2
+	//this is the button
+	var acc = document.getElementsByClassName("course-accordion");
+	var i;
+
+	for (i = 0; i < acc.length; i++) {
+		//when one of the buttons are clicked run this function
+	  acc[i].onclick = function() {
+		//variables
+		var panel = this.nextElementSibling;
+		var coursePanel = document.getElementsByClassName("course-panel");
+		var courseAccordion = document.getElementsByClassName("course-accordion");
+		var courseAccordionActive = document.getElementsByClassName("course-accordion active");
+
+		/*if pannel is already open - minimize*/
+		if (panel.style.maxHeight){
+			//minifies current pannel if already open
+			panel.style.maxHeight = null;
+			//removes the 'active' class as toggle didnt work on browsers minus chrome
+			this.classList.remove("active");
+		} else { //pannel isnt open...
+			//goes through the buttons and removes the 'active' css (+ and -)
+			for (var ii = 0; ii < courseAccordionActive.length; ii++) {
+				courseAccordionActive[ii].classList.remove("active");
+			}
+			//Goes through and removes 'activ' from the css, also minifies any 'panels' that might be open
+			for (var iii = 0; iii < coursePanel.length; iii++) {
+			  this.classList.remove("active");
+			  coursePanel[iii].style.maxHeight = null;
+			}
+		  //opens the specified pannel
+		  panel.style.maxHeight = panel.scrollHeight + "px";
+		  //adds the 'active' addition to the css.
+		  this.classList.add("active");
+		} 
+	  }//closing to the acc onclick function
+	}//closing to the for loop.
